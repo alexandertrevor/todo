2b51578960e5cb7243c83ac9d325a6f831ac6f40
Trevor Mac <trevor.andrew.alexander@gmail.com>
2020-08-17 21:33:50 -0400

    Add todo with ability to complete and delete

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,13 +1,24 @@
 // Check off specific to dos by clicking
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 //Click on X to Delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(500, function(){
         $(this).remove();
     });
     event.stopPropagation();
 });
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        // grabbing new todo text from from input
+        var todoText = $(this).val();
+        // clear input
+        $(this).val("");
+        // create a new li and add to ul
+        $("ul").append("<li><span>x </span>" + todoText + "</li>");
+    }
+})
